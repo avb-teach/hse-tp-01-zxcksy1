6b56c1df39334b11ada3fc8ced12a0fe10973934
zxcksy1 <krismagulova@edu.hse.ru>
2025-04-28 14:09:22 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 input_dir=$1
 output_dir=$2
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+max_depth=1
+for im in "$@"; do
+  if [[ $im == --max_depth=* ]]; then
+    max_depth="${im#*=}"
+  fi
+done
+find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
