c132690a5ed9961b85e2dbbeca9aa49752f745ce
zxcksy1 <krismagulova@edu.hse.ru>
2025-05-01 13:34:39 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -3,15 +3,7 @@ input_dir=$1
 output_dir=$2
 max_depth=""
 
-for meow in "$@"; do
-    if [[ $meow == --max_depth=* ]]; then
-        max_depth="${meow#*=}"
-    fi
-done
 
-if [ -n "$max_depth" ]; then
-    find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
-else
-    find "$input_dir" -type f -exec cp {} "$output_dir" \;
-fi
+find "$input_dir" -type f -exec cp {} "$output_dir" \;
+
 
