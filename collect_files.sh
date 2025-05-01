f1f63456fa0aa54c9edf3c1cbbe0e27aba1225de
zxcksy1 <krismagulova@edu.hse.ru>
2025-05-01 23:15:32 +0500

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,40 +1,28 @@
-
-
 #!/bin/bash
+
 input_dir="$1"
 output_dir="$2"
-max_depth=0  
 
 if [ $# -eq 4 ] && [ "$3" == "--max_depth" ] && [[ "$4" =~ ^[0-9]+$ ]]; then
     max_depth="$4"
-fi
 
-mkdir -p "$output_dir"
-
-find "$input_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
-    rel_path="${file#$input_dir/}"
-    depth=$(tr -cd '/' <<< "$rel_path" | wc -c)
-    depth=$((depth + 1))
-
-    if [ $depth -gt $max_depth ] && [ $max_depth -gt 0 ]; then
-        overflow=$((depth - max_depth))
-        rel_path=$(echo "$rel_path" | awk -F'/' -v o="$overflow" '{
-            for (i=o+1; i<=NF; i++) printf "%s%s", $i, (i<NF?"/":"")
-        }')
-    fi
-
-    dest="$output_dir/$rel_path"
-    mkdir -p "$(dirname "$dest")"
-    
-    counter=1
-    while [ -e "$dest" ]; do
-        if [[ "$dest" =~ \.[^./]+$ ]]; then
-            dest="${dest%.*}_${counter}.${dest##*.}"
-        else
-            dest="${dest}_${counter}"
+    find "$input_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
+        rel_path="${file#$input_dir/}"
+        depth=$(tr -cd '/' <<< "$rel_path" | wc -c)
+        depth=$((depth + 1))
+        
+        if [ $depth -gt $max_depth ]; then
+            overflow=$((depth - max_depth))
+            rel_path=$(echo "$rel_path" | awk -F'/' -v o="$overflow" '{
+                for (i=o+1; i<=NF; i++) printf "%s%s", $i, (i<NF?"/":"")
+            }')
         fi
-        counter=$((counter + 1))
+        
+        dest="$output_dir/$rel_path"
+        mkdir -p "$(dirname "$dest")"
+        cp "$file" "$dest"
     done
+else
 
-    cp "$file" "$dest"
-done
+    find "$input_dir" -type f -exec cp {} "$output_dir" \;
+fi
