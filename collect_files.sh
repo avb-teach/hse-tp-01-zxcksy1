dd9810951c7e14d5c0c7948911c51313d95ae921
zxcksy1 <krismagulova@edu.hse.ru>
2025-05-01 15:40:40 +0500

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+'''#!/bin/bash
 input_dir=$1
 output_dir=$2
 max_depth=""
 
 
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+find "$input_dir" -type f -exec cp {} "$output_dir" \;'''
+
+#!/bin/bash
+if [[ "$1" == "--max_depth" ]]; then
+    max_depth="$2"
+    input_dir="$3"
+    output_dir="$4"
+    find "$input_dir" -mindepth 1 -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
+else
+    input_dir="$1"
+    output_dir="$2"
+    find "$input_dir" -type f -exec cp {} "$output_dir" \;
+fi
 
 
