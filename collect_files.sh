46f5423927abd1b9d366ce4268fcb95ca210055f
zxcksy1 <krismagulova@edu.hse.ru>
2025-05-01 23:25:04 +0500

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -4,27 +4,25 @@ input_dir="$1"
 output_dir="$2"
 
 if [ $# -eq 4 ] && [ "$3" == "--max_depth" ] && [[ "$4" =~ ^[0-9]+$ ]]; then
-    level_limit="$4"
-    while IFS= read -r -d '' source_file; do
-        relative_path="${source_file#$input_dir/}"
+    max_depth="$4"
+
+    while IFS= read -r -d '' file; do
+        rel_path="${file#$input_dir}"
+        rel_path="${rel_path#/}"
         
-        IFS='/' path_segments=($relative_path)
-        
-        reconstructed_path=""
-        current_depth=0
-        for segment in "${path_segments[@]}"; do
-            if [ $current_depth -lt $level_limit ]; then
-                reconstructed_path="${reconstructed_path:+$reconstructed_path/}$segment"
-                current_depth=$((current_depth + 1))
-            else
-                break
+        IFS='/' read -ra path_parts <<< "$rel_path"
+        new_path=""
+        part_count=0
+        for part in "${path_parts[@]}"; do
+            if [ $part_count -lt $max_depth ]; then
+                new_path="${new_path:+$new_path/}$part"
+                part_count=$((part_count + 1))
             fi
         done
         
-        target_path="$output_dir/$reconstructed_path"
-        mkdir -p "${target_path%/*}" 2>/dev/null
-        cp "$source_file" "$target_path" 2>/dev/null
+        mkdir -p "$output_dir/${new_path%/*}" 2>/dev/null
+        cp "$file" "$output_dir/$new_path" 2>/dev/null
     done < <(find "$input_dir" -type f -print0)
 else
-    find "$input_dir" -type f -exec cp --parents {} "$output_dir" \; 2>/dev/null
+    find "$input_dir" -type f -exec cp {} "$output_dir" \; 2>/dev/null
 fi
